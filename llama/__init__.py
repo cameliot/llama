3a554c95e8031f32da17dfeaf0c9b18f4ce8ca0f
Matthias Hannig <matthias@hannig.cc>
2018-02-07 02:29:57 +0100

    started project

---
+++ llama/__init__.py
@@ -0,0 +1,14 @@
+
+"""
+llama - A friendly library to write services on a MQTT bus
+"""
+
+
+__author__ = "Matthias Hannig"
+__copyright__ = "Copyright (c) 2018 Matthias Hannig"
+__credits__ = ["Matthias Hannig"]
+__license__ = "BSD"
+__maintainer__ = "Matthias Hannig"
+__email__ = "matthias@hannig.cc"
+__status__ = "Production"
+
