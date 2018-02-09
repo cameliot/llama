989f8d1f6b8c49cc7c0487442181b795eeff99db
Matthias Hannig <matthias@hannig.cc>
2018-02-09 11:12:06 +0100

    version bump

--- llama/__init__.py
+++ llama/__init__.py
@@ -5,7 +5,7 @@ llama - A friendly library to write services on a MQTT bus
 
 
 __author__ = "Matthias Hannig"
-__version__ = "0.3.5"
+__version__ = "0.4.1"
 __copyright__ = "Copyright (c) 2018 Matthias Hannig"
 __credits__ = ["Matthias Hannig"]
 __license__ = "BSD"
